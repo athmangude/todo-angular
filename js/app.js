0501a0609492648fe9db54e16cfbef8c0f849a74
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2015-05-02 17:39:20 +0300

    >>Adding new tasks to UI

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@
 var TodoApp = angular.module('TodoApp',[]);
 
 TodoApp.controller('TodoController', ['$scope', TodoController]);
-TodoApp.controller('TodoAppController', ['$scope', TodoAppController]);
+TodoApp.controller('TodoAppController', ['$scope', '$rootScope', TodoAppController]);
 
-function TodoAppController($scope){
-	$scope.appTitle = "Todo List "
+function TodoAppController($scope, $rootscope){
+	$scope.appTitle = "Todo List";
+	$scope.addTask = function(form){
+		console.log("Form", $scope.newTask);
+		$rootscope.$broadcast('newTaskAdded', $scope.newTask);
+	}
 }
 
 function TodoController($scope){
@@ -23,4 +27,13 @@ function TodoController($scope){
 		var done = isDone == true ? false : true;
 		$scope.todoList[$index].isDone = done;
 	}
+	
+	$scope.$on('newTaskAdded', function(eventEmitted, newTask){
+		console.log("New task added", newTask);
+		var task = Object.create({
+			task: newTask,
+			isDone: false
+		});
+		$scope.todoList.push(task);
+	});
 }
