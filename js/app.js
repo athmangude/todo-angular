4e495acbd7ce4e84dde91ced46d568a73b11e2c0
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2015-05-02 16:02:53 +0300

    >>initial commit >>Pulling node modules >>Installing Angular >>Using Angular directives

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
-(function (window) {
-	'use strict';
+// Instantiate Angular
+var TodoApp = angular.module('TodoApp',[]);
 
-	// Your starting point. Enjoy the ride!
+TodoApp.controller('TodoController', ['$scope', TodoController]);
 
-})(window);
+function TodoController($scope){
+	$scope.todoList = [
+		{ isDone: true, task: "Learn Angular"},
+		{ isDone: false, task: "Put it in Meteor"},
+		{ isDone: false, task: "Cause a meteor shower"},
+		{ isDone: false, task: "Prepare Spaceship"},
+		{ isDone: false, task: "Fire it Up"},
+		{ isDone: false, task: "Fly to the edge of the Galaxy"}
+	];
+}
