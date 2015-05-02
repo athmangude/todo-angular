d5b432cfbf0725a38b98464d5284ad388a46a821
Athman Gude  CS281-0837/2010 <athmangude@gmail.com>
2015-05-02 16:52:15 +0300

    >>Striking out completed tasks

--- js/app.js
+++ js/app.js
@@ -2,11 +2,16 @@
 var TodoApp = angular.module('TodoApp',[]);
 
 TodoApp.controller('TodoController', ['$scope', TodoController]);
+TodoApp.controller('TodoAppController', ['$scope', TodoAppController]);
+
+function TodoAppController($scope){
+	$scope.appTitle = "Todo List "
+}
 
 function TodoController($scope){
 	$scope.todoList = [
 		{ isDone: true, task: "Learn Angular"},
-		{ isDone: false, task: "Put it in Meteor"},
+		{ isDone: true, task: "Put it in Meteor"},
 		{ isDone: false, task: "Cause a meteor shower"},
 		{ isDone: false, task: "Prepare Spaceship"},
 		{ isDone: false, task: "Fire it Up"},
